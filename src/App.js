769557a2e0120d993864083f1e2821b70ae69b17
Eric P. Katz <ericpkatz@gmail.com>
2017-05-01 05:49:54 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from './components/Nav';
 
 const App = ({ children })=> (
   <div className='container'>
-    <h1>React Redux Template</h1>
+    <h1>React Router Template</h1>
     <Nav />
     { children }
   </div> 
