c818e9805fa163466fd969e0f5ae52021880d9cc
Eric P. Katz <ericpkatz@gmail.com>
2017-05-02 09:30:02 -0400

    return seeded data

--- db.js
+++ db.js
@@ -19,12 +19,27 @@ const sync = ()=> conn.sync({ force: true });
 const seed = ()=> {
   const products = ['foo', 'bar', 'bazz'];
   const users = ['moe', 'larry', 'curly'];
+  let foo, bar, bazz, moe, larry, curly;
 
   return sync()
+    .then(()=> {
+      return User.destroy({ truncate: true });//not sure why i need this?
+    })
     .then(()=> {
       const promises = products.map(name => Product.create({ name }))
         .concat(users.map( name => User.create( { name, password: name.toUpperCase()})));
       return Promise.all(promises);
+    })
+    .then( result => [ foo, bar, bazz, moe, larry, curly ] = result )
+    .then(()=> {
+      return {
+        moe,
+        larry,
+        curly,
+        foo,
+        bar,
+        bazz
+      };
     });
 };
 
